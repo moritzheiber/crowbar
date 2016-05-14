4fbf62bafff54b6612b1cbce9f07c83742aee34c
Thomas Hopkins <thopkins@redventures.com>
2016-05-13 22:52:48 -0400

    add aws config loader fns

--- config-loader.go
+++ config-loader.go
@@ -8,7 +8,7 @@ import "errors"
 
 var badCfgErr = errors.New("Bad configuration file!")
 
-var debugCfg = debug.Debug("config")
+var debugCfg = debug.Debug("oktad:config")
 
 type OktaConfig struct {
 	BaseURL string
@@ -85,4 +85,44 @@ func loadConfig(fname string) (*ini.File, error) {
 	}
 
 	return nil, badCfgErr
+}
+
+// loads the aws profile file, which we need
+// to look up info to assume roles
+func loadAwsCfg() (*ini.File, error) {
+	return ini.Load(
+		fmt.Sprintf(
+			"%s/%s",
+			os.Getenv("HOME"),
+			".aws/config",
+		),
+	)
+}
+
+// reads your AWS config file to load the role ARN
+// for a specific profile; returns the ARN and an error if any
+func readAwsProfile(name string) (string, error) {
+
+	var secondRoleArn string
+	asec, err := loadAwsCfg()
+	if err != nil {
+		debugCfg("aws profile load err, %s", err)
+		return secondRoleArn, err
+	}
+
+	s, err := asec.GetSection(name)
+	if err != nil {
+		debugCfg("aws profile read err, %s", err)
+		return secondRoleArn, err
+	}
+
+	if !s.HasKey("role_arn") {
+		debugCfg("aws profile %s missing role_arn key", name)
+		return secondRoleArn, err
+	}
+
+	arnKey, _ := s.GetKey("role_arn")
+	secondRoleArn = arnKey.String()
+
+	return secondRoleArn, nil
 }
