c6342c5360298a9a0183793c5d96d555a0bc4841
Moritz Heiber <hello@heiber.im>
2020-04-01 14:17:04 +0200

    Forgot to install dbus-x11, make sure the ci script fails early

--- ci/test.sh
+++ ci/test.sh
@@ -4,10 +4,10 @@ OS="${TRAVIS_OS_NAME:-linux}"
 
 if [ "${OS}" == "linux" ]; then
     # https://unix.stackexchange.com/a/548005
-    eval "$(dbus-launch --sh-syntax)"
-    mkdir -p ~/.cache ~/.local/share/keyrings
-    eval "$(printf '\n' | gnome-keyring-daemon --unlock)"
-    eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
+    eval "$(dbus-launch --sh-syntax)" && \
+    mkdir -p ~/.cache ~/.local/share/keyrings && \
+    eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
+    eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)" && \
 
     cargo test --release
 
