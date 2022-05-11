4a59929ef88070ee029e8f7a3d3feea603729050
Moritz Heiber <hello@heiber.im>
2022-05-11 18:31:51 +0200

    Fixed ci script

--- ci/test.sh
+++ ci/test.sh
@@ -5,8 +5,7 @@ if [ "${GITHUB_ACTIONS_OS}" == "linux" ] ; then
     eval "$(dbus-launch --sh-syntax)" && \
     mkdir -p ~/.cache ~/.local/share/keyrings && \
     eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
-    eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)" && \
-
+    eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
 fi
 
 cargo test --release --locked
