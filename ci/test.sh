d061157fd1564cbc12c6442f8b1b7af515921386
Moritz Heiber <hello@heiber.im>
2022-05-11 20:17:15 +0200

    Trying to fix the Linux tests :(

--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,7 @@ set -Eeux -o pipefail
 
 if [ "${GITHUB_ACTIONS_OS}" == "linux" ] ; then
     # https://unix.stackexchange.com/a/548005
-    eval "$(dbus-launch --sh-syntax)" && \
+    export $(dbus-launch) && \
     mkdir -p ~/.cache ~/.local/share/keyrings && \
     eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
     eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
