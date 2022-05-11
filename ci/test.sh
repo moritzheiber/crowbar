da451a22dc44ac7ff0508e71aa05bf5705734e2e
Moritz Heiber <hello@heiber.im>
2022-05-11 21:56:05 +0200

    Lets not use a bash script on anything else but Linux

--- ci/test.sh
+++ ci/test.sh
@@ -2,12 +2,10 @@
 
 set -Eeux -o pipefail
 
-if [ "${GITHUB_ACTIONS_OS}" == "linux" ] ; then
-    # https://unix.stackexchange.com/a/548005
-    export $(dbus-launch) && \
-    mkdir -p ~/.cache ~/.local/share/keyrings && \
-    eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
-    eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
-fi
+# https://unix.stackexchange.com/a/548005
+export $(dbus-launch) && \
+mkdir -p ~/.cache ~/.local/share/keyrings && \
+eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
+eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
 
 cargo test --release --locked
