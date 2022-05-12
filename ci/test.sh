897466fc0de66a4c4a4f3f11ab55e627398f3f97
Moritz Heiber <hello@heiber.im>
2022-05-12 09:41:31 +0200

    Lets wait a little before progressing in CI on Linux

--- ci/test.sh
+++ ci/test.sh
@@ -8,4 +8,6 @@ mkdir -p ~/.cache ~/.local/share/keyrings && \
 eval "$(printf '\n' | gnome-keyring-daemon --unlock)" && \
 eval "$(printf '\n' | /usr/bin/gnome-keyring-daemon --start)"
 
+sleep 5
+
 cargo test --release --locked
